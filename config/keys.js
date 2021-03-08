2c452462c38e7e2277fda3b62f4a902d1fd98e16
amar chinta <amar.chinta@harbingergroup.com>
2021-03-08 16:39:31 +0530

    added debugger and model schema

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-
-mongoURI:'mongodb+srv://Pra07:Mosina07@cluster0.gwnfg.mongodb.net/devconnector?retryWrites=true&w=majority',{ useNewUrlParser: true }
-
+    mongoURI: "mongodb+srv://Pra07:amar@amar@cluster0.gwnfg.mongodb.net/devconnector"
 }
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
-const express =require('express');
-
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const app =express();
-
-//DB config 
- const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
+const detail = require("./models/User");
 
 
-// connect to mongodb 
-mongoose
-        .connect(db)
-        .then(()=> console.log('mongodb connected '))
-        .catch(err => console.log(err));
+mongoose.connect(db, {
+        useNewUrlParser: true,
+}).then((result) => {
+        console.log("done");
+}).catch((err) => {
+        console.log(err);
+});
 
-app.get('/', (req,res) => res.send('hello pradip welcome to my page ') );
+app.get('/', (req, res) => res.send('hello pradip welcome to my page '));
+
+app.get("/addData", (req, res) => {
+        detail.insertMany(
+                [
+                        { name: "Scooby" },
+                        { age: 5 },
+                        { breed: "Great Dane" },
+                        { name: "Rambo" },
+                        { age: 2 },
+                        { breed: "Pitbull" },
+                        { name: "Johny boy" },
+                        { age: 3 },
+                        { breed: "German Shephard" }
+                ],
+                function (err, result) {
+                        if (err) {
+                                res.send(err);
+                        } else {
+                                res.send(result);
+                        }
+                }
+        );
+});
+
+
 
 const port = process.env.PORT || 5000;
 
 
-app.listen(port,() => console.log(`server running on port ${port }`));
+app.listen(port, () => console.log(`server running on port ${port}`));
